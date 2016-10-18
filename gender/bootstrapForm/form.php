cb3306e1e2e48153e9c3bc801bb6f86eda904efa
Ajit Das <ajitdas2900@gmail.com>
2016-10-19 01:59:34 +0600

    assignment file

---
+++ gender/bootstrapForm/form.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Add List Title and Author</title>
+</head>
+<body>
+<form action="" method="">
+    <label for="name">Your Name : </label>
+    <input type="text" name="name">
+    <label for="password">Password : </label>
+    <input type="password" name="password">
+    <input type="submit" value="Login">
+</form>
+</body>
+</html>
